2c2be21259d4c759fb542255853b5b889afdeb5b
Steve Yeom <krazyeom@gmail.com>
2013-04-30 08:04:52 +0900

    Initial Commit

---
+++ DragonRide/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DragonRide
+//
+//  Created by Steve Yeom on 4/30/13.
+//  Copyright Appilogue 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
